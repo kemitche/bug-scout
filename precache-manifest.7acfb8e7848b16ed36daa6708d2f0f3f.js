ba0cded8124a8c0419e2df8b3a5bc26918ea06a5
Keith Mitchell <kemi.krm@gmail.com>
2020-03-28 12:32:27 -0600

    Updates

---
+++ precache-manifest.7acfb8e7848b16ed36daa6708d2f0f3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1272fe00aa6d05d3a4e0761fe26f662",
+    "url": "/bug-scout/index.html"
+  },
+  {
+    "revision": "aba342751a0d8d73642a",
+    "url": "/bug-scout/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a3dd4ead34506683ae1c",
+    "url": "/bug-scout/static/js/2.c4c63708.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bug-scout/static/js/2.c4c63708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba342751a0d8d73642a",
+    "url": "/bug-scout/static/js/main.63e4d807.chunk.js"
+  },
+  {
+    "revision": "1bbe0dbfb1b9892415d0",
+    "url": "/bug-scout/static/js/runtime-main.7c1207b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bug-scout/static/media/logo.5d5d9eef.svg"
+  }
+]);
